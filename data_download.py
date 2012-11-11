9db6b4f916a0c23d21102603725be2a359ac35f9
Sam <samhat87@tamu.edu>
2012-11-10 18:53:42 -0600

    first working version of data download script

--- data_download.py
+++ data_download.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 import requests
 import ujson as json
+import time
 
 def get_song_results(s_params, hundreds=10):
     if 'api_key' not in s_params:
         s_params['api_key'] = 'VSCPXIV4KTWEQ7NFF'
-    s_params['buckets'] = ['audio_summary', 'id:spotify-WW', 'tracks']
+    s_params['bucket'] = ['audio_summary', 'id:spotify-WW', 'tracks']
     s_params['limit'] = 'true'
     s_params['results'] = '100'
 
@@ -17,14 +18,45 @@ def get_song_results(s_params, hundreds=10):
         s_params['start'] = i*100
         raw_results = requests.get(search_url, params=s_params)
         results = raw_results.json
-        if results['response']['status']['code'] <> 0:
+        if results['response']['status']['code'] != 0:
             print 'Search error!'
+            print results['response']['status']
         results_count = len(results['response']['songs'])
+        print "number of results:", results_count
         for j in range(results_count):
+            print j
             song = results['response']['songs'][j]
-            analysis = results.get(song['audio_summary']['analysis_url'])
-            song['audio_summary']['analysis'] = analysis.json
+            detail_url = song['audio_summary']['analysis_url']
+            print detail_url
+            analysis = requests.get(detail_url)
+            print analysis.status_code
+            song['analysis'] = analysis.json
             song['search_rank'] = i*100 + j
+            my_results.append(song)
+        if results_count < 100:
+            break
+
+    return my_results
+
+
+if __name__ == "__main__":
+    params = {"sort": "song_hotttnesss-desc"}
+    
+    start_time = time.time()
+    print "Starting getting results..."
+    results = get_song_results(params, 1)
+    end_time = time.time()
+    print 'Got results after %.3f seconds'%(end_time - start_time)
+
+    start_time = time.time()
+    print "Starting data write..."
+    data_file = open("top_100_songs.json", 'w')
+    json.dump(results, data_file)
+    end_time = time.time()
+    print 'Wrote data after %.3f seconds'%(end_time - start_time)
+    
+            
+            
             
 
     
