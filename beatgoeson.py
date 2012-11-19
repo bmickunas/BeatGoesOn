bc439c5de52c71cfc4a7f59e70fcea1189b3b102
Bradley Mickunas <bmickunas@gmail.com>
2012-11-19 13:45:55 -0600

    the mighty jungle

--- beatgoeson.py
+++ beatgoeson.py
@@ -1,6 +1,7 @@
-DIMENSIONS = ['danceability','duration','energy','key','loudness',
+dim_full_set = ['danceability','duration','energy','key','loudness',
                     'tempo', 'speechiness', 'liveness', 'mode',
                         'time_signature']
+dim_small_set = ['danceability','energy','key'
                             
 class BeatGoesOn(object):
     """ 
@@ -18,11 +19,20 @@ class BeatGoesOn(object):
             # store song in song_space
             self.song_space.append(song);
     def normed_vect(self, song):
+        # Create the vector that has the dimensions with scores
+        vect = {dim:song[dim] for dim in dim_small_set}
+          # calculate the magnitude of the vector
+        # What about the values that are already normalized
+        for dim in dim_small_set:
+            mag = math.sqrt(sum(song[dim]**2))
+        return {dim:weight/mag for dim,score in vect.iteritems()}
         pass
     
     def searchommend(self, song):
-        # calculate similarity value between song and all songs in song_space
-        # store each sim value inside corresponding song object in 'sim'
+        # calculate similarity value between song and all songs 
+        #   in song_space
+        # store each sim value inside corresponding song object 
+        #   in 'sim'
         pass
     def generate_playlist(self, play_count, initial_song):
         # searchomement play_count songs 
