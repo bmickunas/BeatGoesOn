6be0ca227150518b0a6dfc8eb6d8783de1358198
Bradley Mickunas <bmickunas@gmail.com>
2012-11-20 19:05:01 -0600

    the levy passed!

--- beatgoeson.py
+++ beatgoeson.py
@@ -1,7 +1,8 @@
+import math
 dim_full_set = ['danceability','duration','energy','key','loudness',
                     'tempo', 'speechiness', 'liveness', 'mode',
                         'time_signature']
-dim_small_set = ['danceability','energy','tempo']
+dim_small_set = ['danceability','energy','liveness']
                             
 class BeatGoesOn(object):
     """ 
@@ -10,7 +11,7 @@ class BeatGoesOn(object):
     """
     
     def __init__(self):
-        self.song_space = {} # vector space of all songs to choose from        
+        self.song_space = [] # vector space of all songs to choose from        
     def vectorize(self, songs):
         # songs is the list of nice data structures with the info we need
         for song in songs:
@@ -24,14 +25,13 @@ class BeatGoesOn(object):
         vect = {dim:song[dim] for dim in dim_small_set}
           # calculate the magnitude of the vector
         # What about the values that are already normalized?
-        for dim in dim_small_set:
-            mag = math.sqrt(sum(song[dim]**2))
-        return {dim:weight/mag for dim,score in vect.iteritems()}        
+        mag = math.sqrt(sum(song[dim]**2 for dim in dim_small_set))
+        return {dim:score/mag for dim,score in vect.iteritems()}        
     
     def searchommend(self, seed, playlist):
         # calculate similarity value between song and all songs 
         #   in song_space
-        most_sim_song = ['0',0.0]
+        most_sim_song = ['',0.0]
         for song in self.song_space:
             song['sim'] = sum(
                     seed['vect'][dim]*song['vect'][dim]
@@ -39,17 +39,17 @@ class BeatGoesOn(object):
                     )
             # if the song is more similar and it is not already in the playlist
             if ((song['sim'] > most_sim_song[1]) and (playlist.count(song)==0)):                
-                most_sim_song[0] = song['spotify_id']
+                most_sim_song[0] = song
                 most_sim_song[1] = song['sim']
         return most_sim_song[0]        
         
     def generate_playlist(self, play_count, initial_song):
         # searchommend play_count number of songs
         playlist = []
-        result_spot_id = searchommend(initial_song)
+        result = searchommend(initial_song)
         playlist.append(initial_song)
-        playlist.append(result_spot_id)
+        playlist.append(result)
         for i in range(play_count-2):
-            result_spot_id = searchommend(result_spot_id)
-            playlist.append(result_spot_id)
+            result = searchommend(result)
+            playlist.append(result)
         return playlist        
