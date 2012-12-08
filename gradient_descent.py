7c46d81b1b730303b62e8674e5e69e53e8ea4edf
Bradley Mickunas <bmickunas@gmail.com>
2012-12-08 10:52:11 -0600

    pseudo code right now

---
+++ gradient_descent.py
@@ -0,0 +1,11 @@
+
+
+class GradientDescent(object):
+        # <x, y> - training pair where x and y are song vectors
+        # Compute Loss function with parameters x*w and y, where w is the vector of weights
+        # Compute the adjustment to the weights, delta_w = alpha*(dL/dw), learning rate multiplied by the Gradient vector
+        # Update the weights vector, w_2 = w_1 + delta_w
+
+
+
+if __name__ == "__main__":
