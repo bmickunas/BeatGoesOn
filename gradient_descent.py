d1ef3fba2b6346df80fa9a9bd8f0b6a8acb0a60f
Bradley Mickunas <bmickunas@gmail.com>
2012-12-11 15:52:48 -0600

    tests pass for learning

--- gradient_descent.py
+++ gradient_descent.py
@@ -23,8 +23,8 @@ class GradientDescent(object):
         self.model = {} # dict of parameter weights
         for dim in dim_full_set:
             self.model[dim] = 1.0
-        self.learning_rate = 1
-        self.N = 1 # number of song_pairs seen; used form the learning_rate
+        self.learning_rate = 1.0
+        self.N = 1.0 # number of song_pairs seen; used form the learning_rate
     
     def _error(self, input, target_song):
         actual_output = 0.0
@@ -33,26 +33,31 @@ class GradientDescent(object):
             actual_output = actual_output + input['vect'][dim]*self.model[dim]
         for dim in target_song['vect']:
             target_output = target_output + target_song['vect'][dim]
-        return target_output - actual_output
+        return abs(target_output - actual_output)
     
     def _calc_gradient(self, learning_rate, loss, input):
         delta_weight = {}
         for dim in dim_full_set:
-            delta_weight[dim] = learning_rate*loss*input[dim]
+            #print learning_rate," * ", loss, " * ", input['vect'][dim]
+            delta_weight[dim] = learning_rate*loss*input['vect'][dim]
+            #print dim,"_w: ", delta_weight[dim]
+        #print "Delta_w: ",delta_weight
         return delta_weight
         
     def learn(self, song_pairs):
         # <x, y> - training pair where x and y are successful song pairs
-        for pair in song_pairs:            
+        for pair in song_pairs:         
+            # print "Learning from (",pair[0]['title'],", ",pair[1]['title'],")" 
             loss = self._error(pair[0], pair[1])
+            # print "Loss: ", loss
             # Compute the adjustment to the weights, delta_w = alpha*(dL/dw),
-            del_w = self._calc_gradient(self.learning_rate, abs(loss), pair[0])
+            del_w = self._calc_gradient(self.learning_rate, loss, pair[0])
             # Update the weights vector, w_2 = w_1 + delta_w
             for key in self.model.iterkeys():
                 self.model[key] = self.model[key] + del_w[key]
             self.learning_rate = self.learning_rate/(self.N**(0.5))
-            self.N = self.N + 1
-        print self.model
+            self.N = self.N + 1.0
+            # print self.model
                 
 if __name__ == '__main__':
     learner = GradientDescent()
--- gradient_descent_tests.py
+++ gradient_descent_tests.py
@@ -5,28 +5,28 @@ dim_full_set = ['danceability','energy','key','loudness',
                     'tempo', 'speechiness', 'liveness', 'mode',
                         'time_signature']
                         
-equal_pair = ({'key': 2, 'title': 'Diamonds', 'energy': 0.4, 'liveness': 0.1,
+equal_pair = ({'key': 2, 'title': 'equal_0', 'energy': 0.4, 'liveness': 0.1,
  'tempo': 91.9,
  'speechiness': 0.1, 'artist_name': 'Rihanna', 'vect': {'time_signature': 0.5,
  'energy': 0.4, 'liveness': 0.1, 'tempo': 0.3, 'speechiness': 0.1,
  'danceability': 0.5, 'key': 0.2, 'loudness': 0.8, 'mode': 0.0}, 'mode': 0,
  'time_signature': 4, 'duration': 225.2273, 'loudness': -6.7379999999999995,
  'danceability': 0.547264495122005},
- {'key': 2, 'title': 'Diamonds', 'energy': 0.4, 'liveness': 0.1, 'tempo': 91.9,
+ {'key': 2, 'title': 'equal_1', 'energy': 0.4, 'liveness': 0.1, 'tempo': 91.9,
  'speechiness': 0.1, 'artist_name': 'Rihanna', 'vect': {'time_signature': 0.5,
  'energy': 0.4, 'liveness': 0.1, 'tempo': 0.3, 'speechiness': 0.1,
  'danceability': 0.5, 'key': 0.2, 'loudness': 0.8, 'mode': 0.0}, 'mode': 0,
  'time_signature': 4, 'duration': 225.2273, 'loudness': -6.7379999999999995,
  'danceability': 0.547264495122005})
  
-pair_error_of_half = ({'key': 2, 'title': 'Diamonds', 'energy': 0.4, 'liveness': 0.1,
+pair_error_of_half = ({'key': 2, 'title': 'halferror_0', 'energy': 0.4, 'liveness': 0.1,
  'tempo': 91.9,
  'speechiness': 0.1, 'artist_name': 'Rihanna', 'vect': {'time_signature': 0.5,
  'energy': 0.4, 'liveness': 0.1, 'tempo': 0.3, 'speechiness': 0.1,
  'danceability': 0.5, 'key': 0.2, 'loudness': 0.8, 'mode': 0.0}, 'mode': 0,
  'time_signature': 4, 'duration': 225.2273, 'loudness': -6.7379999999999995,
  'danceability': 0.547264495122005},
- {'key': 2, 'title': 'Diamonds', 'energy': 0.4, 'liveness': 0.1, 'tempo': 91.9,
+ {'key': 2, 'title': 'halferror_1', 'energy': 0.4, 'liveness': 0.1, 'tempo': 91.9,
  'speechiness': 0.1, 'artist_name': 'Rihanna', 'vect': {'time_signature': 0.5,
  'energy': 0.4, 'liveness': 0.1, 'tempo': 0.3, 'speechiness': 0.1,
  'danceability': 0.1, 'key': 0.3, 'loudness': 0.6, 'mode': 0.0}, 'mode': 0,
@@ -34,14 +34,17 @@ pair_error_of_half = ({'key': 2, 'title': 'Diamonds', 'energy': 0.4, 'liveness':
  'danceability': 0.547264495122005})
  
 training_set = equal_pair,pair_error_of_half
+training_weights = {'time_signature': 1.25, 'energy': 1.2, 'liveness': 1.05,
+ 'tempo': 1.15, 'speechiness': 1.05, 'danceability': 1.25, 'key': 1.1,
+ 'loudness': 1.4, 'mode': 1.0}
  
 zero_weights = {'time_signature': 0.0, 'energy': 0.0, 'liveness': 0.0,
  'tempo': 0.0, 'speechiness': 0.0, 'danceability': 0.0, 'key': 0.0,
  'loudness': 0.0, 'mode': 0.0}
  
-new_model_one_iter = {'time_signature': 1.25, 'energy': 1.2, 'liveness': 1.05,
- 'tempo': 1.15, 'speechiness': 1.05, 'danceability': 1.25, 'key': 1.1,
- 'loudness': 1.4, 'mode': 0.0}
+delta_weights_one_iter = {'time_signature': 0.25, 'energy': 0.2,
+'liveness': 0.05, 'tempo': 0.15, 'speechiness': 0.05,
+'danceability': 0.25, 'key': 0.1, 'loudness': 0.4, 'mode': 0.0}
  
 class TestGradientDescent(unittest.TestCase):
     def setUp(self):
@@ -49,18 +52,27 @@ class TestGradientDescent(unittest.TestCase):
     def test_ZeroError(self):
         self.assertAlmostEqual(0.0, self.learner._error(equal_pair[0],equal_pair[1]))
     def test_ZeroGradientVector(self):
-        delta_weights = self.learner._calc_gradient(self.learner.learning_rate,
-                                                    0,equal_pair[0])
+        delta_weights = self.learner._calc_gradient(
+                    self.learner.learning_rate,0,equal_pair[0])
         for dim in dim_full_set:
             self.assertEqual(zero_weights[dim], delta_weights[dim])
     def test_Error(self):
         self.assertAlmostEqual(0.5, abs(self.learner._error(pair_error_of_half[0],
                                                             pair_error_of_half[1])))
+    def test_GradientVector(self):
+        delta_weights = self.learner._calc_gradient(
+                1.0, 0.5,
+                    pair_error_of_half[0])
+        print delta_weights
+        for dim in dim_full_set:
+            #print "dim: ", dim
+            self.assertAlmostEqual(delta_weights_one_iter[dim],
+                        delta_weights[dim])
     def test_Learn(self):
         self.learner.learn(training_set)
         for dim in dim_full_set:
-            print dim
-            self.assertAlmostEqual(new_model_one_iter[dim], self.learner.model[dim])
+           # print dim
+            self.assertAlmostEqual(training_weights[dim], self.learner.model[dim])
             
 if __name__ == '__main__':
     unittest.main()            
