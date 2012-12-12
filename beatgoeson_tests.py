85ba222355053fc5ed8b7d9bea801cffc4db0ad5
Bradley Mickunas <bmickunas@gmail.com>
2012-12-12 12:07:23 -0600

    Edited the Readme.txt and deleted the Readme.md

--- beatgoeson_tests.py
+++ beatgoeson_tests.py
@@ -147,6 +147,3 @@ class TestVectorize(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
---
+++ settings_local.py
@@ -0,0 +1,5 @@
+# Settings for this app.
+
+settings = dict(
+    api_key = 'ZSDNTL7YAQRK6028S'
+    )
